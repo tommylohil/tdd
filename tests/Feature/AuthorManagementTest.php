fbe11d688eca06e01c39212551e096a389c201a6
Tommy Lohil <tommylohil23052000@gmail.com>
2019-12-27 23:50:48 +0700

    Created AuthorManagementTest

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/authors', [
+            'name' => 'Author Name',
+            'dob' => '15-12-2019',
+        ]);
+
+        $author = Author::all();
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('2019-12-15', $author->first()->dob->format('Y-m-d'));
+
+    }
+}
