54dfb3bfef1d3ba99da6f2fc481d214cf6f369e6
Tommy Lohil <tommylohil23052000@gmail.com>
2019-12-27 23:50:32 +0700

    Created Author model, migration, and controller

---
+++ app/Author.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    protected $fillable = [
+        'name',
+        'dob',
+    ];
+
+    protected $dates = [
+        'dob'
+    ];
+
+    public function setDobAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob);
+    } 
+}
