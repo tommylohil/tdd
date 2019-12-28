fd7ee1485733e4780cc0ca02d9306dbadde63ec0
tommylohil <tommylohil23052000@gmail.com>
2019-12-29 00:34:25 +0700

    Created Author validation and do refactioring

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -17,15 +17,36 @@ class AuthorManagementTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $this->post('/authors', [
-            'name' => 'Author Name',
-            'dob' => '15-12-2019',
-        ]);
+        $this->post('/authors', $this->data());
 
         $author = Author::all();
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('2019-12-15', $author->first()->dob->format('Y-m-d'));
 
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $res = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+
+        $res->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $res = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $res->assertSessionHasErrors('dob');
+    }
+
+    public function data()
+    {
+        return [
+            'name' => 'Author Name',
+            'dob' => '15-12-2019',
+        ];
     }
 }
